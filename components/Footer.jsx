e837c1b250cedb08ab3b65ea13f4d173f5709f5d
13siddhu <singhsiddhartha220@gmail.com>
2025-12-11 11:47:20 +0530

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
-                { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "+91 9598486555", path: '/', icon: MailIcon },
+                { text: "singhsiddhartha220@gmail.com", path: '/', icon: PhoneIcon },
+                { text: "Ballia ,UP, 277001", path: '/', icon: MapPinIcon }
             ]
         }
     ];
@@ -80,7 +80,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <p className="py-4 text-sm text-slate-500">
-                    Copyright 2025 © gocart All Right Reserved.
+                    Copyright 2025 © Wyre. All Right Reserved.
                 </p>
             </div>
         </footer>
